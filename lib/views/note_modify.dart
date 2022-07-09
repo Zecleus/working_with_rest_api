ca84c9471beb01177f741bc113cbab44dadb750e
Kenneth Cuico <kenneth.cuico123@gmail.com>
2022-07-09 12:25:36 +0800

    Added fetching title and content from API

--- lib/views/note_modify.dart
+++ lib/views/note_modify.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:working_with_rest_api/services/notes_service.dart';
+
+import '../models/note.dart';
 // ignore_for_file: prefer_const_constructors
 
-class NoteModify extends StatelessWidget {
+class NoteModify extends StatefulWidget {
   final String? noteID;
-  bool get isEditing => noteID != null;
-
   NoteModify({this.noteID});
+
+  @override
+  State<NoteModify> createState() => _NoteModifyState();
+}
+
+class _NoteModifyState extends State<NoteModify> {
+  bool get isEditing => widget.noteID != null;
+
+  NotesService get notesService => GetIt.I<NotesService>();
+
+  String? errorMessage;
+  Note? note;
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _isLoading = true;
+    });
+    notesService.getNote(widget.noteID!).then((response) {
+      setState(() {
+        _isLoading = false;
+      });
+      if (response.error!) {
+        errorMessage = response.errorMessage ?? 'An error occured';
+      }
+      note = response.data;
+      _titleController.text = note!.noteTitle!;
+      _contentController.text = note!.noteContent!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,43 +54,47 @@ class NoteModify extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            TextField(
-              decoration: InputDecoration(hintText: 'Note title'),
-            ),
-            Container(
-              height: 8,
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: 'Note content'),
-            ),
-            Container(
-              height: 16,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (isEditing) {
-                    //update note in api
-                  } else {
-                    //create note in api
-                  }
-                  Navigator.of(context).pop();
-                },
-                style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor),
-                child: Text(
-                  'Submit',
-                  style: TextStyle(color: Colors.white),
-                ),
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(hintText: 'Note title'),
+                  ),
+                  Container(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: _contentController,
+                    decoration: InputDecoration(hintText: 'Note content'),
+                  ),
+                  Container(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (isEditing) {
+                          //update note in api
+                        } else {
+                          //create note in api
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor),
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
       ),
     );
   }
